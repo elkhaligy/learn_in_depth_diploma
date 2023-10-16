6b3876d163d1cf1a6cd5608992705387c5c83680
Shehab Elkhaligy <shehabelkhaligy100@gmail.com>
2023-10-17 02:22:29 +0300

    Fixed something

--- Unit 2 C Programming/Lesson 3 C Basics/Assignment 2/example_2.c
+++ Unit 2 C Programming/Lesson 3 C Basics/Assignment 2/example_2.c
@@ -9,7 +9,7 @@ int main() {
     bool v;
     printf("Enter an alphabet: ");
     scanf("%c", &c);
-    v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
     if (v)
         printf("%c is a vowel.", c);
     else
