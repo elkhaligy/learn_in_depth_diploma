45759449737fefae332831b4de9b6914b0756231
Shehab Elkhaligy <shehabelkhaligy100@gmail.com>
2023-11-05 02:57:06 +0200

    Lesson 3 Lab 1 makefile

--- Unit 3 Embedded C/Lesson 3/Lab 1 makefile/startup.s
+++ Unit 3 Embedded C/Lesson 3/Lab 1 makefile/startup.s
@@ -1,7 +1,7 @@
 .global reset // make this reset section global symbol, to be seen later by the linker script
 
 reset:
-	ldr sp, =stack_top //load this value in stack pointer register
+	ldr sp, =stack_top 	//load this value in stack pointer register
 						// stack_top symbol will be calculated by the linker script 
 	bl 	c_is_powerful	// branch label on main
 stop: b stop 			// if you get out of main come here
